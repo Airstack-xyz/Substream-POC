d3cb98960b5d70e141bf161ef78f49e78f0da937
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2022-10-03 12:30:54 +0530

    fixed the substream go client
    There was error in initialising the jwt_token and setting the proper flags for the connection

--- consumer/go/main/main.go
+++ consumer/go/main/main.go
@@ -16,9 +16,9 @@ var jwt_token string
 var endpoint string
 
 func main() {
-	jwt_token := os.Getenv("SUBSTREAMS_API_TOKEN")
+	jwt_token = os.Getenv("SUBSTREAMS_API_TOKEN")
 	if len(jwt_token) == 0 {
-		fmt.Printf("SUBSTREAMS_API_TOKEN not set")
+		fmt.Println("SUBSTREAMS_API_TOKEN not set")
 	}
 	endpoint = "api.streamingfast.io:443"
 	package_pb := "../all-token-tracker-v0.1.0.spkg"
@@ -29,11 +29,10 @@ func main() {
 	manifestReader := manifest.NewReader(package_pb)
 	pkg, err := manifestReader.Read()
 	if err != nil {
-		fmt.Printf("read manifest %q: %w", package_pb, err)
+		fmt.Println("read manifest %q: %w", package_pb, err)
 	}
 
 	client, callOpts := substreams_service()
-	fmt.Println(client)
 	req := &pbsubstreams.Request{
 		StartBlockNum: int64(start_block),
 		StopBlockNum:  uint64(end_block),
@@ -41,12 +40,10 @@ func main() {
 		Modules:       pkg.Modules,
 		OutputModules: output_modules,
 	}
-	fmt.Println("hello")
 	// ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	stream, err := client.Blocks(context.Background(), req, callOpts...)
-	fmt.Println(stream)
 	if err != nil {
-		fmt.Printf("call sf.substreams.v1.Stream/Blocks: %w", err)
+		fmt.Println("call sf.substreams.v1.Stream/Blocks: %w", err)
 	}
 
 	for {
@@ -54,11 +51,10 @@ func main() {
 		fmt.Println(resp)
 		if err != nil {
 			if err == io.EOF {
-				fmt.Printf("Error occured")
+				fmt.Println("Error occured")
 			}
-			fmt.Printf("Error occured in receiving a package: %w", err)
+			fmt.Println("Error occured in receiving a package: %w", err)
 		}
-		fmt.Println(resp.Message)
 		switch r := resp.Message.(type) {
 		case *pbsubstreams.Response_Progress:
 			_ = r.Progress
@@ -69,11 +65,10 @@ func main() {
 		case *pbsubstreams.Response_Data:
 
 			for _, output := range r.Data.Outputs {
-				fmt.Printf("Something")
 				for _, log := range output.Logs {
 					fmt.Println("LOG: ", log)
 				}
-				fmt.Println(output)
+				// fmt.Println(output)
 				// if output.Name == "db_out" {
 				// 	if err := loader.ReturnHandler(output.GetMapOutput().GetValue(), r.Data.Step, r.Data.Cursor, r.Data.Clock); err != nil {
 				// 		fmt.Printf("RETURN HANDLER ERROR: %s\n", err)
@@ -87,11 +82,11 @@ func main() {
 
 func substreams_service() (pbsubstreams.StreamClient, []grpc.CallOption) {
 	ssClient, _, callOpts, err := client.NewSubstreamsClient(
-		endpoint, jwt_token, true, false,
+		endpoint, jwt_token, false, false,
 	)
-	// fmt.Println(err)
+
 	if err != nil {
-		fmt.Printf("Substreams client setup: %w", err)
+		fmt.Println("Substreams client setup: %w", err)
 	}
 
 	return ssClient, callOpts
